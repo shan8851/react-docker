76a30cefe7837eed9d6a9b85db0ed0e6f6701b75
Asam Shan <asamshan456@gmail.com>
2020-02-06 16:00:01 +0000

    initial commit with docker setup for dev and prod

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Dockerized!!!</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.docker.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Docker
         </a>
       </header>
     </div>
